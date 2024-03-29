3338bad476f5e0d46c6cd91790d00efb465c183e
Yu Peng <yu.peng1995@outlook.com>
2024-03-29 13:33:44 -0700

    Added viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
+
+# a standard way for viewset
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     # a standard function to convert api view class to be rendered
     path('hello-view', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
